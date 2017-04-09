b748a653c997d621a5b0eb37256ef81c4577369b
Erik Hedberg <erik@hedberg.at>
2017-04-09 06:25:53 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import App from './App';
+import reducer from './reducers';
+import './index.css';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
