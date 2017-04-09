e38c84e50fb63d5f86136879b06a1bb46f1c4aa9
Erik Hedberg <erik@hedberg.at>
2017-04-09 17:31:58 +0200

    Show feed names in menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import { showMenu, hideMenu } from '../../actions/menu';
 
 const toggleVisibility = (show, hide) => open => {
   open ? show() : hide();
 };
 
-const Menu = ({ visible = false, show, hide }) => {
-  return (
-  <Drawer
-    open={visible}
-    docked={false}
-    onRequestChange={toggleVisibility(show, hide)}
-  />
-)};
+const Menu = ({ visible = false, feedNames, show, hide }) => (
+    <Drawer
+      open={visible}
+      docked={false}
+      onRequestChange={toggleVisibility(show, hide)}
+    >
+      {feedNames.map((name) => (<MenuItem key={name} primaryText={name} />))}
+    </Drawer>
+);
+
+const selectFeedNames = (state) => {
+  const items = state.feedData.items || [];
+  return items.reduce((feedNames, item) => {
+    const { Feed } = item;
+    return feedNames.indexOf(Feed) === -1 ? [...feedNames, Feed] : feedNames;
+  }, [])
+  .sort();
+};
 
 const mapStateToProps = state => ({
+  feedNames: selectFeedNames(state),
   visible: state.menu.visible,
 });
 
